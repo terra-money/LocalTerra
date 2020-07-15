74e54298cec50061bc6d750229266463891a751d
Paul Kim <paul@terra.money>
2020-07-15 23:08:02 +0900

    Add FCD (#2)
    * * Use linux binary for building terracore
    * Adding fcd (WIP)
    
    * Use premade configuration instead of initialize it in Docker
    
    * FCD works
    
    * Update terracore to use dockerhub

--- localterra-terracore/reset.sh
+++
@@ -1,3 +0,0 @@
-terrad add-genesis-account terra1dcegyrekltswvyy0xy69ydgxn9x8x32zdtapd8 1000000000000uluna,1000usd
-echo "123456789\n123456789\n123456789\n123456789\n" | terrad gentx --name test1 --amount 1000000000uluna
-terrad collect-gentxs
