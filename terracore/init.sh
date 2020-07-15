50ab7d327f2099fe6a97ce92b32a173fa1d264d3
Paul Kim <paul@terra.money>
2020-07-15 21:21:34 +0900

    Use premade configuration instead of initialize it in Docker

--- terracore/init.sh
+++
@@ -1,5 +0,0 @@
-terrad init localterra --chain-id=localterra
-terracli keys add test1 --recover < $HOME/mnemonic.txt --keyring-backend test
-terrad add-genesis-account terra1dcegyrekltswvyy0xy69ydgxn9x8x32zdtapd8 1000000000000uluna,1000usd
-terrad gentx --name test1 --amount 1000000000uluna --keyring-backend test
-terrad collect-gentxs
