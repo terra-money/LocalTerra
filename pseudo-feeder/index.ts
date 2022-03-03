terra-money/LocalTerra

commit 71d4693f99d5725e892bae66db402d2b14ed3f4c

fix: pseudo-feeder to use terra.js v3

pseudo-feeder/index.ts
@@ -4,12 +4,11 @@ import {
   LCDClient,
   MnemonicKey,
   MsgAggregateExchangeRateVote,
-  StdFee,
 } from "@terra-money/terra.js";
 
 const {
   MAINNET_LCD_URL = "https://lcd.terra.dev",
-  MAINNET_CHAIN_ID = "columbus-4",
+  MAINNET_CHAIN_ID = "columbus-5",
   TESTNET_LCD_URL = "http://localhost:1317",
   TESTNET_CHAIN_ID = "localterra",
   MNEMONIC = "satisfy adjust timber high purchase tuition stool faith fine install that you unaware feed domain license impose boss human eager hat rent enjoy dawn",
@@ -134,7 +133,7 @@ async function loop() {
     );
 
     const msgs = [lastSuccessVoteMsg, voteMsg.getPrevote()].filter(Boolean);
-    const tx = await wallet.createAndSignTx({ msgs, fee: new StdFee(200000, '30000uusd') });
+    const tx = await wallet.createAndSignTx({ msgs });
 
     await testnetClient.tx
       .broadcast(tx)

docker-compose.yml
@@ -15,7 +15,7 @@ services:
       - "9091:9091"
     command: terrad start
   oracle:
-    image: terramoney/pseudo-feeder:bombay
+    image: terramoney/pseudo-feeder:0.5.5
     pull_policy: always
     depends_on:
       - terrad
@@ -40,7 +40,7 @@ services:
   #   ports:
   #     - "6379:6379"
   fcd-collector:
-    image: terramoney/fcd:1.0.10
+    image: terramoney/fcd:1.0.11
     depends_on:
       - terrad
       - postgres
@@ -52,7 +52,7 @@ services:
     command: collector
     restart: unless-stopped
   fcd-api:
-    image: terramoney/fcd:1.0.10
+    image: terramoney/fcd:1.0.11
     depends_on:
       - terrad
       - postgres

commit 46fe8a7fd55eee8870cfe1fe52bc459ad9583d80

explicit fee for price feeder
fixes a regression in https://github.com/terra-money/LocalTerra/commit/71d4693f99d5725e892bae66db402d2b14ed3f4c#diff-54855b3c211c1c73c2545a70b576ed8395ebb84115d405061d2f4255700e8210R136
see also https://github.com/terra-money/LocalTerra/issues/64

pseudo-feeder/index.ts
@@ -4,6 +4,7 @@ import {
   LCDClient,
   MnemonicKey,
   MsgAggregateExchangeRateVote,
+  Fee
 } from "@terra-money/terra.js";
 
 const {
@@ -133,7 +134,7 @@ async function loop() {
     );
 
     const msgs = [lastSuccessVoteMsg, voteMsg.getPrevote()].filter(Boolean);
-    const tx = await wallet.createAndSignTx({ msgs });
+    const tx = await wallet.createAndSignTx({ msgs, fee: new Fee(200000, '30000uusd') });
 
     await testnetClient.tx
       .broadcast(tx)